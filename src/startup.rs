3d62705f9a76054d00dfaa08ee0fb8d9595d5d54
JonShort <JonShort@users.noreply.github.com>
2022-03-16 15:12:12 +0000

    [refactor] - prep dir structure for future

---
+++ src/startup.rs
@@ -0,0 +1,13 @@
+use actix_web::dev::Server;
+use actix_web::{App, HttpServer};
+use std::net::TcpListener;
+
+use crate::routes::{health_check, subscribe};
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
+        .listen(listener)?
+        .run();
+
+    Ok(server)
+}
